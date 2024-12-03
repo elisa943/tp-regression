a66259a3c7c5489737315ece4c736c0b2f4de626
Alban Oberti <alban.oberti@bordeaux-inp.fr>
2024-12-03 15:58:29 +0100

    rl data2

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 # Régression Linéaire 
-
+def Reg_lin(X,y):
+    coeff=np.linalg.inv(np.dot(np.transpose(X),X))
+    coeff=np.dot(coeff,np.transpose(X))
+    coeff=np.dot(coeff,y)
+    return coeff
 # OLS
 data1 = np.load('data1.npy')
 len_data1 = len(data1[0, :])
 
+
 x = np.array(data1[0, :])
 y = np.array(data1[1, :])
+
+X=[[1,x[i]] for i in range(len(x))]
 
 x_data = x
 y_data = y
@@ -30,13 +37,15 @@ regr = LinearRegression()
 
 # Train the model using the training sets
 regr.fit(x_train, y_train)
-
+a=Reg_lin(X,y)
+print(a)
 # Make predictions using the testing set
 y_pred = regr.predict(x_test)
 
 # Plot outputs
 plt.scatter(x, y, color="black")
 plt.plot(x_test, y_pred, color="blue", linewidth=3)
+plt.plot(x_test,a[1]*x_test+a[0], color="red")
 
 plt.show()
 
@@ -47,9 +56,46 @@ erreur_apprentissage = 1 / len_data1 * sum(l_OLS(y_train, y_pred))
 
 print("L'erreur d'apprentissage est de : ", erreur_apprentissage)
 
+data2 = np.load('data2.npy')
+len_data2 = len(data2[0, :])
 
+x = np.array(data2[0, :])
+y = np.array(data2[1, :])
 
+X=[[1,x[i]] for i in range(len(x))]
 
+x_data = x
+y_data = y
 
+# Use only one feature
+x_data = x_data[:, np.newaxis]
+
+# Split the data into training/testing sets
+x_train = x_data[:-len_data2//2]
+x_test = x_data[-len_data2//2:]
+
+# Split the targets into training/testing sets
+y_train = y_data[:-len_data2//2]
+y_test = y_data[-len_data2//2:]
+
+# Create linear regression object
+regr = LinearRegression()
+a=Reg_lin(X,y)
+print(a)
+# Train the model using the training sets
+regr.fit(x_train, y_train)
+
+# Make predictions using the testing set
+y_pred = regr.predict(x_test)
+
+# Plot outputs
+plt.scatter(x, y, color="black")
+plt.plot(x_test, y_pred, color="blue", linewidth=3)
+plt.plot(x_test,a[1]*x_test+a[0], color="red")
+plt.show()
+
+erreur_apprentissage = 1 / len_data2 * sum(l_OLS(y_train, y_pred))
+
+print("L'erreur d'apprentissage est de : ", erreur_apprentissage)
 
 # Ridge
